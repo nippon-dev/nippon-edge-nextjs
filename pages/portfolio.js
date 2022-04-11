37f490cdf71049dc9984f6745e53be404f4cb9a3
Kevin Miller Jr <kevinmillerjr@gmail.com>
2022-04-11 22:28:07 +0700

    Minor commit

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -119,6 +119,7 @@ export default function Portfolios() {
         <>
 {/** Start of Header Section */}
 <div className="relative bg-swhite mb-4 sm:mb-12">
+  
       <div className="absolute inset-0">
         <img
           className="w-full h-full object-cover"
@@ -130,7 +131,7 @@ export default function Portfolios() {
       <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
         <h1 className="text-4xl font-extrabold tracking-tight text-swhite sm:text-5xl lg:text-6xl">Portfolio</h1>
         <p className="mt-6 text-xl text-gray-100 max-w-3xl">
-        What have developed in Jamstack and WordPress in the past.
+        We have experience with Jamstack and WordPress.
         </p>
       </div>
     </div>
@@ -138,6 +139,53 @@ export default function Portfolios() {
 {/** Start of Main Body Section */}
 <div className="relative py-16 bg-swhite overflow-hidden">
 <Meta title="Nippon Edge WordPress and Jamstack website portfolios" />
+<div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+        <div className="relative h-full text-lg max-w-prose mx-auto" aria-hidden="true">
+          <svg
+            className="absolute top-12 left-full transform translate-x-32"
+            width={404}
+            height={384}
+            fill="none"
+            viewBox="0 0 404 384"
+          >
+            <defs>
+              <pattern
+                id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+                x={0}
+                y={0}
+                width={20}
+                height={20}
+                patternUnits="userSpaceOnUse"
+              >
+                <rect x={0} y={0} width={4} height={4} className="text-sblue" fill="currentColor" />
+              </pattern>
+            </defs>
+            <rect width={404} height={384} fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
+          </svg>
+
+          <svg
+            className="absolute bottom-12 left-full transform translate-x-32"
+            width={404}
+            height={384}
+            fill="none"
+            viewBox="0 0 404 384"
+          >
+            <defs>
+              <pattern
+                id="d3eb07ae-5182-43e6-857d-35c643af9034"
+                x={0}
+                y={0}
+                width={20}
+                height={20}
+                patternUnits="userSpaceOnUse"
+              >
+                <rect x={0} y={0} width={4} height={4} className="text-sblue" fill="currentColor" />
+              </pattern>
+            </defs>
+            <rect width={404} height={384} fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
+          </svg>
+        </div>
+      </div>
       <div className="relative px-4 sm:px-6 lg:px-8 border-b-2 border-b-sorange border-dotted">
         <div className="text-lg max-w-prose mx-auto">
           <h1>
