ae536161a24490a19240264a83d7c1d0f69747b6
Kevin Miller Jr <kevinmillerjr@gmail.com>
2022-04-12 12:36:52 +0700

    Updated Meta

--- components/Meta.js
+++ components/Meta.js
@@ -62,7 +62,7 @@ const Meta = ({ title, keywords, description, siteTitle }) => {
       <link rel="icon" type="image/png" sizes="192x192"  href="/android-chrome-512x512.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="manifest" href="/sitemanifest.json" />
+      <link rel="manifest" href="/site.webmanifest" />
       <title>{`${title} | ${siteTitle}`}</title>
     </Head>
   )
