406a10cca291f8e0a687a8bf1f3bfcd99b841f18
Kevin Miller Jr <kevinmillerjr@gmail.com>
2022-04-24 07:16:53 +0700

    Removed site from portfolio

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -52,15 +52,6 @@ const currentPorts = [
     location: 'HCMC, Vietnam',
     imageSrc: 'https://res.cloudinary.com/shinkirin/image/upload/v1650271857/kevin-matsunaga/singapore-node.webp',
     imageAlt: 'Nippon Edge\'s Singapore Node website.',
-  },
-  {
-    id: 5,
-    framework: 'Next.js + Tailwind CSS',
-    href: 'https://gohalalvietnam.com',
-    company: 'GoHalalVietnam!',
-    location: 'HCMC, Vietnam',
-    imageSrc: 'https://res.cloudinary.com/shinkirin/image/upload/v1649729164/kevin-matsunaga/gohalalvietnam-screenshot.webp',
-    imageAlt: 'GoHalalVietnam! Next.js + Tailwind CSS website.',
   }
 ]
 
